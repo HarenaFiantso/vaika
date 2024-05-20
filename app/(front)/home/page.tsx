987ec6c5f5b0b62845adae44b4c2e9bd966994fe
HarenaFiantso <fiantsorav@gmail.com>
2024-05-20 19:03:30 +0300

    chore: root layout redirection to /home

---
+++ app/(front)/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+  return <h1>Front office home page</h1>;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
-export default function Home() {
-  return <h1>Hello from Vaika !</h1>;
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function Root() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/home');
+  }, [router]);
+
+  return null;
 }
