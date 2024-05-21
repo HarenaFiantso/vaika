18be8b04a07670ae02145fd3d6418e6a3b5123b9
HarenaFiantso <fiantsorav@gmail.com>
2024-05-21 04:51:31 +0300

    chore: add header links data

---
+++ lib/data.ts
@@ -0,0 +1,18 @@
+export const links = [
+  {
+    name: 'Home',
+    hash: '#home',
+  },
+  {
+    name: 'About',
+    hash: '#about',
+  },
+  {
+    name: 'Explore cars',
+    hash: '#explore_cars',
+  },
+  {
+    name: 'Contact',
+    hash: '#contact',
+  },
+] as const;
