0ab3620d79c9562f157e109b41a5098601fbf850
HarenaFiantso <fiantsorav@gmail.com>
2024-05-21 04:22:05 +0300

    fix: rm duplication

--- app/(front)/home/page.tsx
+++ app/(front)/home/page.tsx
@@ -1,3 +1,3 @@
-export default function Home() {
+export default function HomePage() {
   return <h1>Front office home page</h1>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function Root() {
+export default function RootPage() {
   const router = useRouter();
 
   useEffect(() => {
