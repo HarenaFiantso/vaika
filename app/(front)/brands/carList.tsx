adc5ce77a9ac38cb93dadb22921dfc5101165450
Tombosoa <hei.nantenaina.4@gmail.com>
2024-05-23 14:45:32 +0300

    chore: mapping mock json

--- app/(front)/brands/carList.tsx
+++ app/(front)/brands/carList.tsx
@@ -1,15 +1,31 @@
+import mock from "@/brandCarMock.json";
 interface brandProps{
     brands: string;
 }
 
 export default function CarsBrandList({brands}: brandProps){
     return(
-        <div>
-            <h1 className="text-xl font-bold">Our {brands} Cars </h1>
+        <div className="w-[70%] h-full">
+            <h1 className="text-xl font-bold">Our {brands} cars ({mock.length} results) </h1>
             <p className="text-xs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi natus in voluptatum illo nostrum ipsum consequatur magnam quo id cupiditate, dolor earum accusantium facere voluptate quia expedita.</p>
             <div className="h-0.5 border-t border-gray-400 my-5"></div>
-            <div>
-                list of car with the same brand
+            <div className="grid grid-cols-4 gap-3 overflow-auto h-[80%] ">
+                {mock.map((car, index) => (
+                    <div key={index} className="rounded-md overflow-auto h-[max-content] bg-white shadow-md w-[80%]">
+                        <div className="overflow-hidden">
+                            <img src={ car.img } alt="car's image" className="w-[100%]" />
+                        </div>
+                        <div className="p-2">
+                            <p className="font-bold text-xs"> {car.name} </p>
+                            <p className="text-xs"> { car.brand } </p>
+                            <p className="text-xs" > { car.kilometers } </p>
+
+                            <div className="mt-[1rem]">
+                                <p className="font-bold text-xs"> {car.price} </p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
