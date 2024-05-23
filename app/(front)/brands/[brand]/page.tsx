9380edbca449dbebac7b9bada12cba71a04d4d8e
Tombosoa <hei.nantenaina.4@gmail.com>
2024-05-23 14:46:08 +0300

    chore: style++

--- app/(front)/brands/[brand]/page.tsx
+++ app/(front)/brands/[brand]/page.tsx
@@ -13,13 +13,14 @@ interface Car {
 const BrandPage: React.FC = () => {
     const {brand} = useParams();  
     return(
-        <div className="bg-gray-200 w-full h-[70vh]">
-            <div className="flex gap-3 p-5">
-                <div className="w-[30%] p-4 bg-white">
+        <div className="bg-gray-100 w-full h-[85vh]">
+            <div className="flex p-6 justify-evenly w-full h-[100%]">
+                <div className="w-[25%] p-4 bg-white h-[max-content] rounded-md">
                     <h1 className="font-bold">Filter</h1>
                     <AccordionComponent  title="Keyword" content="search by keyword"/>
                     <AccordionComponent  title="Price" content="search by price"/>
                     <AccordionComponent  title="Kilometers" content="idk"/>
+                    <AccordionComponent  title="Name" content="idk"/>
                 </div>
                 <CarsBrandList brands={brand} />
             </div>
