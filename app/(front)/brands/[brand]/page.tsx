4baac387e84f0b7cc19a618c5089cf035c4cb539
Tombosoa <hei.nantenaina.4@gmail.com>
2024-05-23 13:27:28 +0300

    chore: import and use accordion component

--- app/(front)/brands/[brand]/page.tsx
+++ app/(front)/brands/[brand]/page.tsx
@@ -2,6 +2,8 @@
 
 import { useParams } from "next/navigation";
 import React from "react";
+import CarsBrandList from "../carList";
+import AccordionComponent from "@/components/accordion";
 
 interface Car {
     name: string,
@@ -11,7 +13,17 @@ interface Car {
 const BrandPage: React.FC = () => {
     const {brand} = useParams();  
     return(
-        <div>Car with brand {brand} </div>
+        <div className="bg-gray-200 w-full h-[70vh]">
+            <div className="flex gap-3 p-5">
+                <div className="w-[30%] p-4 bg-white">
+                    <h1 className="font-bold">Filter</h1>
+                    <AccordionComponent  title="Keyword" content="search by keyword"/>
+                    <AccordionComponent  title="Price" content="search by price"/>
+                    <AccordionComponent  title="Kilometers" content="idk"/>
+                </div>
+                <CarsBrandList brands={brand} />
+            </div>
+        </div>
     )
 }
 export default BrandPage;
