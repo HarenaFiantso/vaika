32e49115839f13a374698de121fe9ee196247788
HarenaFiantso <fiantsorav@gmail.com>
2024-05-23 19:01:21 +0300

    refactor: format code

--- app/(front)/layout.tsx
+++ app/(front)/layout.tsx
@@ -1,5 +1,7 @@
-import { Footer, Navbar } from '@/components';
 import { ReactNode } from 'react';
+
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
   return (
