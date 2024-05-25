3b9f0f7a8f9ae0ea4c8d586179e449c4864dabe3
HarenaFiantso <fiantsorav@gmail.com>
2024-05-25 09:05:49 +0300

    chore: main structure ui of hero implemented

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,16 @@
 import Image from 'next/image';
 import { ReactNode } from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function Hero(): ReactNode {
-  return <h1>Hero section</h1>;
+  return (
+    <section className="hero-pattern w-full h-[875px]">
+      <div className="max-w-[1080px] m-auto h-full flex flex-col gap-10 items-center justify-center">
+        <h1 className="text-5xl font-semibold">Find the car that searching for you.</h1>
+        <div>
+          <Button className="bg-[#E83657]">Button</Button>
+        </div>
+      </div>
+    </section>
+  )
 }
