59030c0191b10993ad5f9d4e2c458ac77c347347
HarenaFiantso <fiantsorav@gmail.com>
2024-05-25 09:06:39 +0300

    chore: add class-variance-authority

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config = {
       },
     },
     extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/public/img/tesla.png')"
+      },
       colors: {
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
