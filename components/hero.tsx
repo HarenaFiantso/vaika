bbdaa24f4ff4d074d693008f3e7815c88f73b4ad
HarenaFiantso <fiantsorav@gmail.com>
2024-05-25 09:19:39 +0300

    style: hero section styles

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,16 +1,14 @@
-import Image from 'next/image';
 import { ReactNode } from 'react';
-import { Button } from '@/components/ui/button';
 
 export default function Hero(): ReactNode {
   return (
     <section className="hero-pattern w-full h-[875px]">
-      <div className="max-w-[1080px] m-auto h-full flex flex-col gap-10 items-center justify-center">
-        <h1 className="text-5xl font-semibold">Find the car that searching for you.</h1>
-        <div>
-          <Button className="bg-[#E83657]">Button</Button>
-        </div>
+      <div className="max-w-[1536px] m-auto h-full flex flex-col gap-10 items-start justify-center">
+        <h1 className="text-8xl font-semibold">Find the car that <br /> searching for you.</h1>
+        <button
+          className="bg-[#E83657] w-[500px] rounded-lg text-lg py-5 px-10 hover:bg-white hover:text-black transition-all">Button
+        </button>
       </div>
     </section>
-  )
+  );
 }
