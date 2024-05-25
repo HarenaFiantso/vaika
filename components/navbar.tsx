f71887148d4d1bde8c64e6ccca1b16b55390c7f5
HarenaFiantso <fiantsorav@gmail.com>
2024-05-25 09:06:01 +0300

    style: change link color of navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,35 +1,39 @@
 import Link from 'next/link';
 import { ReactNode } from 'react';
+import { CircleUserIcon } from 'lucide-react';
+import { Menu } from '@mui/icons-material';
 
 export default function Navbar(): ReactNode {
   return (
-    <header className='fixed top-0 w-full border-b-2'>
+    <header className='fixed top-0 w-full backdrop-blur-lg'>
       <nav className='mx-auto flex max-w-[1536px] items-center justify-between gap-5 py-5'>
         <Link
           href={'/home'}
-          className='flex-2 border-r-2 px-5 text-[30px] font-semibold tracking-wider text-[#E83657] '
+          className='flex-2 px-5 text-[30px] font-semibold tracking-wider text-[#E83657] '
         >
           Vaika
         </Link>
 
         {/* TODO: Map using constants */}
         <ul className='flex flex-1 gap-10'>
-          <li className='text-[#332E69]'>
+          <li className='text-white'>
             <Link href=''>Find your car</Link>
           </li>
-          <li className='text-[#332E69]'>
+          <li className='text-white'>
             <Link href=''>Sell or trade in</Link>
           </li>
-          <li className='text-[#332E69]'>
+          <li className='text-white'>
             <Link href=''>How it works</Link>
           </li>
-          <li className='text-[#332E69]'>
+          <li className='text-white'>
             <Link href=''>Car Finance</Link>
           </li>
         </ul>
 
         {/* TODO: Use shadcn button */}
-        <button className='rounded-full border-2 border-[#E83657] px-10 py-2 text-[#E83657]'>Sign In</button>
+        <button className='rounded-full flex gap-2 border-2 border-[#E83657] px-5 py-2 text-[#E83657]'>
+          <Menu className="text-white" /> <CircleUserIcon/>
+        </button>
       </nav>
     </header>
   );
