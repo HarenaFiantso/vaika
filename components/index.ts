c5a0129916601822a9c6bcdf2ebf1a22f5da88b4
HarenaFiantso <fiantsorav@gmail.com>
2024-05-28 19:04:54 +0300

    style: hero section

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export { default as Navbar } from './navbar';
 export { default as CustomButton } from './customButton';
 export { default as Loader } from './loader';
+export { default as Hero } from './hero';
