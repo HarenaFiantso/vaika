00e0f2c154fbad8dc0a2c55c7cadbd9ce6f17f45
HarenaFiantso <fiantsorav@gmail.com>
2024-05-28 20:27:56 +0300

    feat: create new car catalogue section

--- app/(front)/home/page.tsx
+++ app/(front)/home/page.tsx
@@ -1,15 +1,21 @@
-import { Hero } from '@/components';
+'use client';
+
+import { Catalogue, Hero } from '@/components';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <main className='relative overflow-hidden backdrop-blur-3xl backdrop-filter dark:bg-slate-900'>
-      <div className='absolute right-1/2 top-3/4 hidden h-44 w-44 rotate-12 rounded-full from-pink-500 to-purple-700 blur-3xl md:flex dark:bg-gradient-radial' />
-      <div className='absolute left-1/2 top-[98%] h-44 w-44 rotate-12 rounded-2xl from-blue-500 to-violet-950 blur-3xl dark:bg-gradient-radial' />
-      <div className='absolute left-1/2 top-1/2 h-72 w-72 rotate-12 rounded-2xl from-indigo-900 to-purple-700 blur-3xl dark:bg-gradient-radial' />
-      <div className='absolute left-4 top-0 h-20 w-72 rotate-12 rounded-2xl from-teal-500 to-green-700 blur-3xl dark:bg-gradient-radial' />
-      <div className='absolute right-0 top-[15%] h-72 w-72 rotate-12 rounded-2xl from-slate-700 to-[#343434] blur-3xl dark:bg-gradient-radial' />
+      <div className='absolute right-1/2 top-3/4 hidden h-44 w-44 rotate-12 rounded-full from-indigo-500 to-violet-950 blur-3xl md:flex dark:bg-gradient-radial' />
+      <div className='absolute left-1/2 top-[98%] h-44 w-44 rotate-12 rounded-2xl from-pink-500 to-pink-950 blur-3xl dark:bg-gradient-radial' />
+      <div className='absolute left-1/2 top-1/2 h-72 w-72 rotate-12 rounded-2xl from-indigo-900 to-pink-700 blur-3xl dark:bg-gradient-radial' />
+      <div className='absolute left-4 top-0 h-20 w-72 rotate-12 rounded-2xl from-indigo-500 to-indigo-700 blur-3xl dark:bg-gradient-radial' />
+      <div className='absolute right-0 top-[15%] h-72 w-72 rotate-12 rounded-2xl from-pink-700 to-pink-600 blur-3xl dark:bg-gradient-radial' />
 
       <Hero />
+      <Catalogue isLoading={isLoading} />
     </main>
   );
 }
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,7 @@ export { default as Navbar } from './navbar';
 export { default as CustomButton } from './customButton';
 export { default as Loader } from './loader';
 export { default as Hero } from './hero';
+export { default as Catalogue } from './catalogue';
+export { default as SearchBar } from './searchBar';
+export { default as Filter } from './filter';
+export { default as CustomSelect } from './customSelect';
