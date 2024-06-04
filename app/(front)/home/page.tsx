f72d96a6d25cd53c6adf7738693081162c4dc063
HarenaFiantso <fiantsorav@gmail.com>
2024-06-04 20:56:56 +0300

    fix: home page catalogue

--- app/(front)/home/page.tsx
+++ app/(front)/home/page.tsx
@@ -1,10 +1,28 @@
 'use client';
 
 import { Catalogue, Hero } from '@/components';
-import { useState } from 'react';
+import { carApi } from '@/services/vaika-api';
+import { Car } from '@vaika-api/typescript-client';
+import { useEffect, useState } from 'react';
 
-export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+type FetchCarProps = {
+  size?: number;
+};
+
+type HomeProps = {
+  searchParams: FetchCarProps;
+};
+
+export default function Home({ searchParams }: HomeProps) {
+  const { size } = searchParams;
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [allCars, setAllCars] = useState<Car[] | any>([]);
+
+  useEffect((): void => {
+    setIsLoading(true);
+    carApi.getCars().then((data) => setAllCars(data));
+    setIsLoading(false);
+  }, []);
 
   return (
     <main className='relative overflow-hidden backdrop-blur-3xl backdrop-filter dark:bg-slate-900'>
@@ -15,7 +33,7 @@ export default function Home() {
       <div className='absolute right-0 top-[15%] h-72 w-72 rotate-12 rounded-2xl from-pink-700 to-pink-600 blur-3xl dark:bg-gradient-radial' />
 
       <Hero />
-      <Catalogue isLoading={isLoading} />
+      {allCars && <Catalogue allCars={allCars} isLoading={isLoading} size={(size || 20) / 10} />}
     </main>
   );
 }
