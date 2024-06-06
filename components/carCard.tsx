6be32bf3984171882c35b3c059cdfa894567db51
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-06 09:06:33 +0300

    chore: use data response

--- components/carCard.tsx
+++ components/carCard.tsx
@@ -1,12 +1,12 @@
-'use client'
+'use client';
 
-import { Car } from '@vaika-api/typescript-client';
 import CustomButton from './customButton';
+import { Car } from '@vaika-api/typescript-client';
 import { useRouter } from 'next/navigation';
 
 export default function carCard({ car }: { car: Car }) {
   const router = useRouter();
-  return(
+  return (
     <div className='group h-fit  w-full max-w-lg rounded-2xl border bg-white from-slate-700 to-slate-900   p-3 transition-all duration-150 ease-linear md:p-4 md:hover:border md:hover:shadow-lg dark:border-zinc-600 dark:bg-gradient-radial dark:text-slate-300'>
       <div className='flex items-center justify-between'>
         <h1 className=' w-28 max-w-[75%] truncate text-lg font-bold capitalize md:text-xl '> {car.type?.name} </h1>
@@ -30,14 +30,14 @@ export default function carCard({ car }: { car: Car }) {
         </div>
         <div className='mt-6 flex items-center justify-between'>
           <div className='relative '> {car.price} $</div>
-          <CustomButton 
-          title='Details'
-          type='submit'
-          handleClick={()=>router.push(`/appointment/${car.id}`)}
-          containerStyle=' mx-left mt-[1rem] w-[7rem] bg-blue-600 text-white px-6 border rounded-full dark:bg-slate-700 dark:text-slate-300 dark:border-slate-600'
+          <CustomButton
+            title='Details'
+            type='submit'
+            handleClick={() => router.push(`/appointment/${car.id}`)}
+            containerStyle=' mx-left mt-[1rem] w-[7rem] bg-blue-600 text-white px-6 border rounded-full dark:bg-slate-700 dark:text-slate-300 dark:border-slate-600'
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
