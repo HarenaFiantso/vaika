59431ccb6142b5a3fb62b1a7fd2c7ed25867b9dc
Tendry24 <hei.tendry.2@gmail.com>
2024-06-08 01:18:35 +0300

    refactor: remove default ListGuesser and EditGuesser

--- components/dashboard/adminApp.tsx
+++ components/dashboard/adminApp.tsx
@@ -4,15 +4,13 @@ import users from '@/operations/users';
 import LoginPage from './loginPage';
 import { authProvider } from '@/providers/authProvider';
 import dataProvider from '@/providers/dataProvider';
-import { Admin, EditGuesser, ListGuesser, Resource } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
 import brands from '@/operations/brands';
 import cars from '@/operations/cars';
 
 const AdminApp = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider} loginPage={LoginPage} requireAuth>
     <Resource name='users' {...users} />
-    <Resource name='posts' list={ListGuesser} edit={EditGuesser} recordRepresentation='title' />
-    <Resource name='comments' list={ListGuesser} edit={EditGuesser} />
     <Resource name='cars' {...cars}/>
     <Resource name='brands' {...brands} />
   </Admin>
