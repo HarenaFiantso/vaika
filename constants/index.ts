20cbf1aad40846ad676a99d494843c7e819bd49e
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-07 22:34:59 +0300

    chore: id_car -> car_id

--- constants/index.ts
+++ constants/index.ts
@@ -36,7 +36,7 @@ export const Appointment = z
     contact: z.string().min(10).max(10),
     message: z.string().min(5),
     appointment_datetime: z.coerce.date(),
-    id_car: z.string(),
+    car_id: z.string(),
   })
   .required();
 
