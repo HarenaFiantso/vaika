ef1d17b0f38cf0cf5a1f571e40914ea026a6bdeb
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-08 03:38:11 +0300

    chore: update login page

--- components/Breadcrumbs/Breadcrumb.tsx
+++ components/Breadcrumbs/Breadcrumb.tsx
@@ -4,19 +4,19 @@ interface BreadcrumbProps {
 }
 const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
   return (
-    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      <h2 className="text-title-md2 font-semibold text-black dark:text-white">
+    <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between bg-[#0b1120]">
+      <h2 className="text-title-md2 font-semibold text-white">
         {pageName}
       </h2>
 
       <nav>
         <ol className="flex items-center gap-2">
           <li>
-            <Link className="font-medium" href="/admin">
+            <Link className="font-medium text-white" href="/admin">
               Admin /
             </Link>
           </li>
-          <li className="font-medium text-primary">{pageName}</li>
+          <li className="font-medium text-primary text-white">{pageName}</li>
         </ol>
       </nav>
     </div>
