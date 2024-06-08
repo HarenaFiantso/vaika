f5e1d8708dd5d6c392017e40dcb128cf3260eabf
HarenaFiantso <fiantsorav@gmail.com>
2024-06-08 05:02:17 +0300

    fix: merge conflict

--- utils/cache.ts
+++ utils/cache.ts
@@ -33,6 +33,7 @@ const createObjectCacher = <T>(key: string, storageFactory: () => Storage) => {
 };
 
 const inLocalStorage = <T>(key: string) =>
+  // @ts-ignore
   createObjectCacher<T>(key, () => {
     if (typeof window !== 'undefined') {
       return window?.localStorage;
