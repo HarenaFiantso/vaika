c53363bd94bd683043d25e4d77a79f0bef172699
tanjonaaa <hei.tanjona@gmail.com>
2024-06-08 09:47:49 +0300

    feat: show brands

--- operations/brands/BrandList.tsx
+++ operations/brands/BrandList.tsx
@@ -1,11 +1,11 @@
-import { Datagrid, List, TextField } from 'react-admin';
+import { Datagrid, ImageField, List, TextField } from 'react-admin';
 
 export const BrandList = () => (
   <List>
     <Datagrid rowClick='edit'>
       <TextField source='id' />
       <TextField source='name' />
-      <TextField source='logoUrl' />
+      <ImageField source='logo_url' />
     </Datagrid>
   </List>
 );
