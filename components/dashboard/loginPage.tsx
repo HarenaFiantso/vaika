75fbdaf002d2a38dbc60571e8c89bc3d0f8815de
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-08 03:38:11 +0300

    chore: update login page

--- components/dashboard/loginPage.tsx
+++ components/dashboard/loginPage.tsx
@@ -36,7 +36,6 @@ export default function LoginPage() {
                 </h1>
               </Link>
               <p className="2xl:px-20 text-white">
-
                 Vaika admin is an app to manage a 'vaika' as admin.
               </p>
 
@@ -168,14 +167,12 @@ export default function LoginPage() {
           <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
             <div className="w-full p-4 sm:p-12.5 xl:p-17.5">
               <h2 className="mb-9 text-2xl font-bold text-white sm:text-title-xl2">
-
                 Sign In to Vaika Admin
               </h2>
 
               <form onSubmit={handleSubmit}>
                 <div className="mb-4">
                   <label className="mb-2.5 block font-medium text-white">
-
                     Email
                   </label>
                   <div className="relative">
@@ -185,7 +182,6 @@ export default function LoginPage() {
                       onChange={e => setEmail(e.target.value)}
                       placeholder="Enter your email"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none  text-white dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-
                     />
 
                     <span className="absolute right-4 top-4">
@@ -210,7 +206,6 @@ export default function LoginPage() {
 
                 <div className="mb-6">
                   <label className="mb-2.5 block font-medium text-white">
-
                      Password
                   </label>
                   <div className="relative">
@@ -220,7 +215,6 @@ export default function LoginPage() {
                     onChange={e => setPassword(e.target.value)}
                       placeholder="6+ Characters, 1 Capital letter"
                       className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none text-white dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
-
                     />
 
                     <span className="absolute right-4 top-4">
@@ -252,7 +246,6 @@ export default function LoginPage() {
                     type="submit"
                     value="Sign In"
                     className="w-full cursor-pointer rounded-lg border border-primary bg-primary p-4 text-white transition hover:bg-opacity-90"
-
                   />
                 </div>
               </form>
