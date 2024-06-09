75070bbc12df9352daa56b811dff3aebfd4aecce
HarenaFiantso <fiantsorav@gmail.com>
2024-06-09 17:53:26 +0300

    style: aos

--- components/common/navbar.tsx
+++ components/common/navbar.tsx
@@ -12,11 +12,11 @@ const navLinks = [
     label: 'Home',
   },
   {
-    href: '#about',
+    href: '/about',
     label: 'About',
   },
   {
-    href: '#contact',
+    href: '/contact',
     label: 'Contact us',
   },
   {
@@ -30,19 +30,19 @@ export default function Navbar() {
   const router = useRouter();
 
   return (
-    <header className='flex items-center justify-around border-b px-4 py-4'>
-      <Link href={'/home'} className='flex items-center gap-2 text-4xl font-bold'>
+    <header className="z-40 flex sticky bg-white top-0 items-center justify-around border-b px-4 py-4">
+      <Link href={'/home'} className="flex items-center gap-2 text-4xl font-bold">
         Vaika
       </Link>
 
       <nav>
-        <ul className='flex gap-x-10 text-[16px]'>
-          {navLinks.map((link) => (
-            <li key={link.href}>
+        <ul className="flex gap-x-10 text-[16px]">
+          {navLinks.map((link, index) => (
+            <li key={index}>
               <Link
                 className={`${
                   pathname === link.href ? 'font-semibold text-zinc-900' : 'text-zinc-400'
-                } transition-all hover:text-zinc-900`}
+                } transition-all hover:text-zinc-900 cursor-pointer`}
                 href={link.href}
               >
                 {link.label}
@@ -51,8 +51,8 @@ export default function Navbar() {
           ))}
         </ul>
       </nav>
-
-      <Button onClick={() => router.push('/admin')} className='flex gap-x-5 rounded-full px-10 py-7 text-[16px]'>
+      <Button onClick={() => router.push('/admin')}
+              className="flex gap-x-2 px-10 py-7 text-[16px]">
         <LoginOutlined /> Login
       </Button>
     </header>
