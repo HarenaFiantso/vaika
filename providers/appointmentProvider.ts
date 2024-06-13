c3ac0989a594b0ffd1bda713aec64f41a00c9735
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-13 12:29:59 +0300

    chore: any -> CrupdateAppointment

--- providers/appointmentProvider.ts
+++ providers/appointmentProvider.ts
@@ -1,13 +1,14 @@
 import { AppointmentStatusEnum } from '@vaika-api/typescript-client';
 import { VaikaDataProvider } from './VaikaDataProvider';
 import { brandApi, carApi, unwrap, appointmentApi } from '@/services/vaika-api';
+import { CrupdateAppointment } from '@vaika-api/typescript-client';
 
 export const appointmentProvider: VaikaDataProvider = {
     getOne: async function (id: string, meta?: any): Promise<any> {
         return await unwrap(() => appointmentApi.getAppointmentById(id));
     },
-    saveOrUpdate: async function (resources: any, meta?: any): Promise<any> {
-        return await unwrap(() => appointmentApi.crupdateAppointment(resources));
+    saveOrUpdate: async function (resources: CrupdateAppointment, meta?: any): Promise<any> {
+        return await unwrap(() => appointmentApi.crupdateAppointment([resources]));
     },
     delete: function (id: string): Promise<any> {
         throw new Error('Function not implemented.');
