c705433e2240183cd14bd7dad2897d3f7497a781
Tombosoa <hei.nantenaina.4@gmail.com>
2024-06-13 15:11:47 +0300

    chore: update appointmentProvider

--- providers/appointmentProvider.ts
+++ providers/appointmentProvider.ts
@@ -2,19 +2,49 @@ import { AppointmentStatusEnum } from '@vaika-api/typescript-client';
 import { VaikaDataProvider } from './VaikaDataProvider';
 import { brandApi, carApi, unwrap, appointmentApi } from '@/services/vaika-api';
 import { CrupdateAppointment } from '@vaika-api/typescript-client';
+import { Appointment } from '@vaika-api/typescript-client';
+
+const transformAppointments = (appointments: Appointment[]): any[] => {
+    return appointments.map(appointment => {
+        return {
+            id: appointment.id,
+            last_name: appointment.last_name,
+            first_name: appointment.first_name,
+            email: appointment.email,
+            contact: appointment.contact,
+            message: appointment.message,
+            appointment_datetime: appointment.appointment_datetime,
+            status: appointment.status,
+            car_id: appointment.car?.id
+        };
+    });
+};
 
 export const appointmentProvider: VaikaDataProvider = {
     getOne: async function (id: string, meta?: any): Promise<any> {
         return await unwrap(() => appointmentApi.getAppointmentById(id));
     },
-    saveOrUpdate: async function (resources: CrupdateAppointment, meta?: any): Promise<any> {
-        return await unwrap(() => appointmentApi.crupdateAppointment([resources]));
-    },
+    saveOrUpdate: async function (resources: [CrupdateAppointment], meta?: any): Promise<any> {
+       const transformedResources = transformAppointments(resources);
+         return await unwrap(() => appointmentApi.crupdateAppointment(transformedResources));
+    },    
     delete: function (id: string): Promise<any> {
         throw new Error('Function not implemented.');
     },
     getList: async function (page?: number, perPage?: number, filter?: any, meta?: any): Promise<any[]> {
-        const appointments = await unwrap(() => appointmentApi.getAppointments(AppointmentStatusEnum.PENDING || AppointmentStatusEnum.ARCHIVED || AppointmentStatusEnum.REJECTED || AppointmentStatusEnum.VALIDATED));
-        return appointments;
+        const statusesToFetch = [
+            AppointmentStatusEnum.PENDING,
+            AppointmentStatusEnum.ARCHIVED,
+            AppointmentStatusEnum.VALIDATED,
+            AppointmentStatusEnum.REJECTED
+        ];
+        const promises = statusesToFetch.map(status => {
+            return unwrap(() => appointmentApi.getAppointments(status));
+        });
+        const appointmentsArrays = await Promise.all(promises);
+
+        const allAppointments = appointmentsArrays.flat();
+    
+        return allAppointments;
     }
 }
