cfa17bd00526db596e697dd3c306c63ea5960961
tanjonaaa <hei.tanjona@gmail.com>
2024-06-14 20:30:29 +0300

    feat: appointments change status

--- operations/appointment/AppointmentList.tsx
+++ operations/appointment/AppointmentList.tsx
@@ -3,12 +3,9 @@ import { Datagrid, List, TextField } from 'react-admin';
 export const AppointmentList = () => (
   <List>
     <Datagrid rowClick='edit'>
-      <TextField source='id' />
       <TextField source='last_name' />
       <TextField source='first_name' />
       <TextField source='email' />
-      <TextField source='contact' />
-      <TextField source='message' />
       <TextField source='appointment_datetime' />
       <TextField source='status' />
     </Datagrid>
