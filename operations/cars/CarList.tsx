80554c309e1c629cc57f359a04afdaafac7fcb36
tanjonaaa <hei.tanjona@gmail.com>
2024-06-14 18:22:00 +0300

    feat: can edit car type

--- operations/cars/CarList.tsx
+++ operations/cars/CarList.tsx
@@ -2,13 +2,14 @@ import { Datagrid, ImageField, List, TextField } from 'react-admin';
 
 export const CarList = () => (
   <List>
-    <Datagrid rowClick="edit">
-      <TextField source="name" />
-      <TextField source="brand.name" />
-      <TextField source="price" />
-      <TextField source="model" />
-      <TextField source="power" />
-      <ImageField source="images[0].url" title="Featured image" />
+    <Datagrid rowClick='edit'>
+      <TextField source='name' />
+      <TextField source='brand.name' />
+      <TextField source='type.name' />
+      <TextField source='price' />
+      <TextField source='model' />
+      <TextField source='power' />
+      <ImageField source='images[0].url' label='Featured image' />
     </Datagrid>
   </List>
 );
